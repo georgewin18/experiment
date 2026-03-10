7ca4bcb7aebeb1eff40295e5a92daea106b85c88
georgewin18 <georgewk1809@gmail.com>
2026-03-11 01:00:07 +0700

    feat(main): edit print text

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "V1 - Halo Dunia Tech Masa Depan!\n")
+	fmt.Fprintf(w, "V2 - Hello Future!\n")
 }
 
 func main() {
